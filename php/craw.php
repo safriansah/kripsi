82dbb03c133d41bc811093c522bf100ccddd869d
Muhammad Syafriansyah <safriansah@gmail.com>
2019-02-27 15:07:03 +0700

    update template luminoadmin

--- php/craw.php
+++ php/craw.php
@@ -1,14 +1,34 @@
 <?php
 include 'koneksi.php';
-if($_POST['simpan']){
-    $kategori=$_POST['kategori'];
-    if($kategori=="ekonomi") $url="https://ekonomi.kompas.com/search/all/";
-    else if($kategori=="bola") $url="https://bola.kompas.com/search/all/";
-    else if($kategori=="tekno") $url="https://tekno.kompas.com/search/all/";
-    else if($kategori=="entertainment") $url="https://entertainment.kompas.com/search/all/";
-    getUrl($koneksi, $kategori, $url, 1, 1);
+
+if($_POST['crawl']){
+    crawlBerita($koneksi);
+}
+
+if($_POST['reset']){
+    resetTabel($koneksi);
 }
 header("location:../");
+
+function resetTabel($koneksi){
+    mysqli_query($koneksi, "TRUNCATE TABLE tb_berita");
+}
+
+function crawlBerita($koneksi){
+    $kategori=$_POST['kategori'];
+    $eurl="";
+    $data = mysqli_query($koneksi,"select url from tb_berita where kategori='".$kategori."'");
+    while($d = mysqli_fetch_array($data)){
+        $eurl.=$d['url']." ";
+    }
+    if($kategori=="ekonomi") $url="https://ekonomi.kompas.com/search/all/";
+    else if($kategori=="olahraga") $url="https://bola.kompas.com/search/all/";
+    else if($kategori=="teknologi") $url="https://tekno.kompas.com/search/all/";
+    else if($kategori=="entertainment") $url="https://entertainment.kompas.com/search/all/";
+    if($kategori=="ekonomi") getEkonomi($koneksi, $kategori, $url, 1, 1, $eurl);
+    else getUrl($koneksi, $kategori, $url, 1, 1, $eurl);
+    //echo $kategori." ".$url." ".$eurl;
+}
 
 function bacaHTML($url){
     // inisialisasi CURL
@@ -33,13 +53,12 @@ function cekKata($string, $kata){
     return $hasil;
 }
 
-function getBerita($koneksi, $url, $kategori){
+function getBerita($url, $kategori){
     //$kodeHTML =  bacaHTML($url);
     $kodeHTML = file_get_contents($url);
     $pecah = explode('<h1 class="read__title">', $kodeHTML);
     $judul = explode('</h1>', $pecah[1]);
     $judul = $judul[0];
-
     $pecah = explode('<div class="read__content">', $kodeHTML);
     $isi = explode('<div class="fb-quote">', $pecah[1]);
     $isi = $isi[0];
@@ -59,26 +78,57 @@ function getBerita($koneksi, $url, $kategori){
     include 'koneksi.php';
     $isi = str_replace("'"," ",$isi);
     $isi = str_replace('"',' ',$isi);
-    $query="insert into tb_berita values('','$url','$judul','$isi','$kategori')";
-    mysqli_query($koneksi, $query);
-    //echo $query."<hr>";
+    $query="insert into tb_berita values('','$url','$judul','$isi','$kategori',NOW())";
+    //mysqli_query($koneksi, $query);
+    return $query;
 }
 
-function getUrl($koneksi, $kategori, $url, $page, $jumlah){
-    $kodeHTML =  bacaHTML($url.$page);
-    $pecah = explode('<a class="article__link" href="', $kodeHTML);
-    $i=0;
-    foreach($pecah as $a){
-        if($i==0){
+function getUrl($koneksi, $kategori, $url, $page, $jumlah, $exist){
+    while($jumlah<10){
+        $kodeHTML =  bacaHTML($url.$page);
+        //$kodeHTML = file_get_contents($url.$page);
+        $pecah = explode('<a class="article__link" href="', $kodeHTML);
+        $i=0;
+        foreach($pecah as $a){
+            if($i==0){
+                $i++;
+                continue;
+            }
+            $a=explode('"', $a);
             $i++;
-            continue;
+            if(!cekKata($exist, $a[0])){
+                mysqli_query($koneksi, getBerita($a[0], $kategori));
+                //echo $a[0];
+                $jumlah++;
+            }
         }
-        $a= explode('"', $a);
-        $i++;
-		$jumlah++;
-		getBerita($koneksi, $a[0], $kategori);
+        echo $page." : ".$jumlah."<hr>";
+        $page+=1;
     }
-    if($jumlah<10) getUrl($koneksi, $kategori, $url, $page++, $jumlah);
+}
+
+function getEkonomi($koneksi, $kategori, $url, $page, $jumlah, $exist){
+    while($jumlah<10){
+        $kodeHTML =  bacaHTML($url.$page);
+        //$kodeHTML = file_get_contents($url.$page);
+        $pecah = explode('<div class="terkini__post">', $kodeHTML);
+        $i=0;
+        foreach($pecah as $a){
+            if($i==0){
+                $i++;
+                continue;
+            }
+            $a=explode('<a href="', $a);
+            $a=explode('"', $a[1]);
+            $i++;
+            if(!cekKata($exist, $a[0])){
+                mysqli_query($koneksi, getBerita($a[0], $kategori));
+                //echo $a[0];
+                $jumlah++;
+            }
+        }
+        echo $page." : ".$jumlah."<hr>";
+        $page+=1;
+    }
 }
 ?>
-
